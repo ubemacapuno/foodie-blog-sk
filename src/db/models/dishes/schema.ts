dcedde76b6d0551ae3d6cd5a7354574944690d53
Fast DMS Corey <corey.damocles@fastdms.com>
2023-03-26 21:22:04 -0700

    feat(Updated dishes Model): Actions, collections, schema etc.

--- src/db/models/dishes/schema.ts
+++ src/db/models/dishes/schema.ts
@@ -3,25 +3,23 @@ import { z } from 'zod';
 import { zodToJsonSchema } from 'zod-to-json-schema';
 
 // Validation schema for dishes
-export const capas_schema = z.object({
-	_id: z.string(),
-	name: z.string().min(1),
-	dateCreated: z.string(),
-	capaStatus: z.enum(statuses),
-	capaPhase: z.enum(phases),
-	problemStatement: z.string().min(1),
-	dateCapaApproved: z.string(),
-	currentPhaseDueDate: z.string(),
-	productImpacted: z.string(),
-	//TODO: Figure out why z.date() doesn't accept the date-string provided from frontend:
-	//TODO: Why is the default fallback not working?
-	documentCreated: z.string().default(() => `Test default string`)
+
+export const dishes_schema = z.object({
+	name: z.string().nonempty(),
+	dateAdded: z.date().default(() => new Date()),
+	dateCooked: z.date().default(() => new Date()),
+	rating: z.string().nonempty(),
+	ingredients: z.array(z.string()).optional(),
+	instructions: z.string().optional().default(''),
+	cuisine: z.string().optional(),
+	notes: z.string().optional().default(''),
+	userId: z.string().nonempty().optional().default('')
 });
 
-export const capas_raw_schema_json = zodToJsonSchema(capas_schema, {
+export const dishes_raw_schema_json = zodToJsonSchema(dishes_schema, {
 	$refStrategy: 'none'
 });
 
-export type Capa = z.infer<typeof capas_schema>;
+export type Dish = z.infer<typeof dishes_schema>;
 
-export const capas_json_schema = zod_to_jsonschema(capas_raw_schema_json);
+export const dishes_json_schema = zod_to_jsonschema(dishes_raw_schema_json);
