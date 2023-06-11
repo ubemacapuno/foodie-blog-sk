ac76bf70ed5b1334a967486e0c85c36dc1dee6cc
Fast DMS Corey <corey.damocles@fastdms.com>
2023-06-11 15:35:55 -0700

    feat(Dash and Package Updates): Updated packages and dashboard

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,33 +1,42 @@
 <script lang="ts">
 	import type { PageData } from './$types';
 	import { superForm } from 'sveltekit-superforms/client';
+	import { format } from 'date-fns';
 
 	export let data: PageData;
+	// Stats props:
+	export let totalDishes: Number | String;
+	export let totalUsers: Number | String;
+	export let featuredDish: String;
+	export let cuisine: String;
 
+	const currentDateFormatted = format(new Date(), 'PP');
 	const taintedMessage: string | null | false =
 		'Do you want to leave this page? Changes you made may not be saved.';
 	// Client API:
 	const { form, errors, constraints, enhance } = superForm(data.form, { taintedMessage });
 </script>
 
-<div class="flex justify-center items-center flex-col">
+<div class="stats stats-vertical shadow">
 	<h1 class="text-3xl my-2">Home - Coming Soon!</h1>
-	<div class="stats bg-neutral text-primary-content">
-		<div class="stat">
-			<div class="stat-title">Account balance</div>
-			<div class="stat-value text-accent">$89,400</div>
-			<div class="stat-actions">
-				<button class="btn btn-sm btn-primary">Add funds</button>
-			</div>
-		</div>
 
-		<div class="stat">
-			<div class="stat-title">Current balance</div>
-			<div class="stat-value text-accent">$89,400</div>
-			<div class="stat-actions">
-				<button class="btn btn-sm btn-secondary">Withdrawal</button>
-				<button class="btn btn-sm btn-secondary">deposit</button>
-			</div>
+	<div class="stat">
+		<div class="text-primary stat-title">Total Dishes</div>
+		<div class="stat-value text-primary">{totalDishes || '-'}</div>
+		<div class="stat-desc">Mar 25, 2023 - {currentDateFormatted}</div>
+	</div>
+
+	<div class="stat">
+		<div class="text-secondary stat-title">Total Users</div>
+		<div class="stat-value text-secondary">{totalUsers || '-'}</div>
+		<div class="stat-desc">Mar 25, 2023 - {currentDateFormatted}</div>
+	</div>
+
+	<div class="stat">
+		<div class="text-accent stat-title">Featured Dish</div>
+		<div class="stat-value text-accent">{featuredDish || '-'}</div>
+		<div class="stat-desc">
+			{cuisine || '-'}
 		</div>
 	</div>
 </div>
--- src/routes/authenticated/dashboard/+page.svelte
+++ src/routes/authenticated/dashboard/+page.svelte
@@ -1,20 +1,53 @@
+<script lang="ts">
+	import { page } from '$app/stores';
+	import { avatarLetters } from '$utilities/helpers';
+
+	//Stat Props:
+	export let totalDishes: Number | String;
+	export let totalUserDishes: Number | String;
+
+	const avatarInitials = avatarLetters(String($page.data.session?.user?.name));
+</script>
+
 <div class="flex justify-center items-center flex-col">
 	<h1 class="text-3xl my-2">Your Dashboard - Coming Soon!</h1>
-	<div class="stats bg-neutral text-primary-content">
-		<div class="stat">
-			<div class="stat-title">Total Dishes</div>
-			<div class="stat-value text-primary">xx</div>
-			<div class="stat-actions">
-				<button disabled class="btn btn-sm btn-primary">View</button>
+	<div class="flex flex-col justify-start items-start">
+		<div class="card-body p-0 gap-0">
+			<div class="avatar">
+				{#if $page.data.session?.user?.image}
+					<div class="w-24 rounded-xl">
+						<img src={$page.data.session?.user?.image} alt="User avatar." />
+					</div>
+				{:else}
+					<div class="bg-neutral-focus text-neutral-content rounded-full w-16">
+						<span class="text-xl">{avatarInitials}</span>
+					</div>
+				{/if}
 			</div>
+			<p>
+				<span class="text-primary">{$page.data.session?.user?.name ?? 'Username Not Found'}</span>
+			</p>
+			<p>
+				<span class="text-secondary">{$page.data.session?.user?.email ?? 'Email Not Found'}</span>
+			</p>
 		</div>
 
-		<div class="stat">
-			<div class="stat-title">Your Dishes</div>
-			<div class="stat-value text-accent">xx</div>
-			<div class="stat-actions">
-				<button disabled class="btn btn-sm btn-secondary">View</button>
-				<button disabled class="btn btn-sm btn-secondary">Create</button>
+		<div class="stats bg-neutral text-primary-content">
+			<div class="stat">
+				<div class="stat-title">Total Dishes</div>
+				<div class="stat-value text-primary">{totalDishes || '-'}</div>
+				<div class="stat-actions">
+					<button disabled class="btn btn-sm btn-primary">View</button>
+				</div>
+			</div>
+
+			<div class="stat">
+				<div class="stat-title">Your Dishes</div>
+				<div class="stat-value text-accent">{totalUserDishes || '-'}</div>
+				<div class="stat-actions">
+					<button disabled class="btn btn-sm btn-secondary">View</button>
+					<button disabled class="btn btn-sm btn-secondary">Create</button>
+				</div>
 			</div>
 		</div>
 	</div>
